0a41bebc0ff938bf39452cbfe8c7f94353126a47
ryiefreaks <officialarnav4@gmail.com>
2025-11-28 15:35:30 +0530

    Add files via upload

---
+++ vote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    
+    if (age >= 18) {
+        printf("User is eligible to vote.\n");
+    } else {
+        printf("User is not eligible to vote.\n");
+    }
+    
+    return 0;
+}
